ce0b7247abcb41356c461e550ae87db0a3ee1879
Marko Bolliger <sh4fty@gmail.com>
2023-02-11 15:02:53 +0100

    remove histoire since it breaks

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-const { $client } = useNuxtApp()
-const test = ref<string>('gerg')
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <input type="text" v-model="test" />
-  </div>
+  <div></div>
 </template>
